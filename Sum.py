b991d4921556c7686042be8906362b16311b9762
Jashwanth Kumar Sathish Kumar <Jashwanthkumar0912@gmail.com>
2025-10-24 07:43:44 +0100

    Add files via upload

---
+++ Sum.py
@@ -0,0 +1,7 @@
+#Ask the user to enter two whole numbers.  Add the numbers together and display the answer to the user.
+
+number_1 = int(input("Enter the first whole number: "))
+number_2 = int(input("Enter the second whole number: "))
+total = 0
+total = number_1 + number_2
+print(total)
