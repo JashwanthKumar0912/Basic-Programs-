040bef02e4833a44ff494acdc627e864df2c1024
Jashwanth Kumar Sathish Kumar <Jashwanthkumar0912@gmail.com>
2025-10-24 07:27:14 +0100

    Add files via upload

---
+++ Bignumber.py
@@ -0,0 +1,12 @@
+#Your program must ask the user to enter a whole number.
+
+#If the number is greater than 100, display the message "That's a big number".
+
+#At the end of the program, display "Thank you".
+
+Number = int(input("Enter a whole number: "))
+if Number > 100:
+    print("That's a big number")
+    print("Thank you")
+else:
+    print("Thank you")
---
+++ lengthfunction.py
@@ -0,0 +1,9 @@
+#Ask the user to enter their name.
+
+#Use the Len() function to find the number of characters in their name.
+
+#Display the length to the user.
+
+Name = str(input("Enter your name: "))
+Name = str(len(Name))
+print("Your name has " + (Name) + " characters.")
